c261386615625e245c1cbd334cc67dca3394aa27
Trinkfass Lisa <Trinkfass.Lisa@sus.htl-grieskirchen.at>
2020-11-02 23:05:51 +0100

    checkSudoku funktioniert

--- Sudoku/src/sudoku/Main.java
+++ Sudoku/src/sudoku/Main.java
@@ -13,17 +13,29 @@ import java.io.File;
 
 public class Main {
 
+    private static final int[][] STUDENT_SUDOKU_WITH_ONLY_ONE_SOLUTION_SOLVED = new int[][]{
+        {1, 7, 5, 9, 8, 2, 3, 6, 4},
+        {9, 4, 6, 3, 7, 1, 5, 2, 8},
+        {3, 8, 2, 4, 5, 6, 7, 1, 9},
+        {8, 2, 7, 5, 3, 9, 1, 4, 6},
+        {5, 9, 4, 6, 1, 8, 2, 3, 7},
+        {6, 1, 3, 7, 2, 4, 8, 9, 5},
+        {4, 3, 1, 8, 9, 7, 6, 5, 2},
+        {2, 6, 8, 1, 4, 5, 9, 7, 3},
+        {7, 5, 9, 2, 6, 3, 4, 8, 1}
+    };
+
     public static void main(String[] args) {
         SudokuSolver ss = new SudokuSolver();
         int[][] input = ss.readSudoku(new File("1_sudoku_level1.csv"));
 
         System.out.println(">--- ORIGINAL ---");
-        // print the sudoku if you want
-        //      int[][] output = ss.solveSudoku(input);
+        //print the sudoku if you want
+        int[][] output = ss.solveSudoku(input);
         System.out.println(">--- SOLUTION ---");
         // print the sudoku if you want
         System.out.println(">----------------");
-        System.out.println("SOLVED    = " + ss.checkSudoku(input));
+        System.out.println("SOLVED    = " + ss.checkSudoku(output));
         System.out.println(">----------------");
     }
 }
--- Sudoku/src/sudoku/SudokuSolver.java
+++ Sudoku/src/sudoku/SudokuSolver.java
@@ -25,12 +25,15 @@ import java.util.logging.Logger;
  */
 public class SudokuSolver implements ISodukoSolver {
 
+    private int[][] solution;
+
     public SudokuSolver() {
-        //initialize if necessary
+        this.solution = new int[9][9];
+
     }
 
     @Override
-    public final int[][] readSudoku(File file) {
+    public final int[][] readSudoku(File file) { //fertig
         int[][] felder = new int[9][9];
         String line;
         int a = 0;
@@ -65,15 +68,18 @@ public class SudokuSolver implements ISodukoSolver {
             }
         }
 
-        for (int j = 0; j < rawSudoku[1].length; j++) { // checkt spalte
-            for (int a = 0; a < rawSudoku.length; a++) {
-                reihe[a] = rawSudoku[j][a];
+        reihe = new int[9];
+        for (int zeile = 0; zeile < rawSudoku.length; zeile++) { // checkt spalte
+            for (int spalte = 0; spalte < rawSudoku.length; spalte++) {
+                reihe[spalte] = rawSudoku[zeile][spalte];
             }
             if (!check9Felder(reihe)) {
                 return false;
             }
         }
-        int row = 0;
+
+        reihe = new int[9];
+        int row = 0;  //cehck 9erblock
         int spalte = 0;
         int aktuelleStelle = 0;
         for (int x = 0; x < 9; x++) {
@@ -111,53 +117,122 @@ public class SudokuSolver implements ISodukoSolver {
 
     @Override
     public int[][] solveSudoku(int[][] rawSudoku) {
-
-        return new int[0][0];
-    }
-
-    @Override
-    public int[][] solveSudokuParallel(int[][] rawSudoku) {
         int[][][] arrOptions = new int[9][9][9];
-        int[][] solution = new int[9][9];
         int[] block = new int[9];
+
         for (int i = 0; i < rawSudoku.length; i++) {
             for (int j = 0; j < rawSudoku.length; j++) {
-                if (rawSudoku[i][j] == 0) {
-                    solution[i][j] = rawSudoku[i][j];
+                if (rawSudoku[i][j] != 0) {
+                    this.solution[i][j] = rawSudoku[i][j];
                 } else {
                     for (int k = 0; k < rawSudoku.length; k++) {
-                        arrOptions[i][j][k] = k;
+                        arrOptions[i][j][k] = k + 1;   //befüllen von arrOption mit allen Ziffern (1-9)
                     }
                 }
             }
         }
 
-        for (int i = 0; i < solution.length; i++) {
-            for (int j = 0; j < solution.length; j++) {
-                if (solution[i][j] != 0) {
-                    block = solution[i];
+        while (checkSudoku(solution) != true) {
+            //Überprüfung der Reihen
+            for (int i = 0; i < this.solution.length; i++) {
+                for (int j = 0; j < this.solution.length; j++) {
+                    if (this.solution[i][j] == 0) {  //überprüfung ob Stelle schon fixe Zahl hat
+                        block = this.solution[i];
 
+                        for (int k = 0; k < this.solution.length; k++) {
+                            if (arrOptions[i][j][k] != 0) { //Überprüfung ob die Option nicht schon gelöscht wurde
+                                if (existenz(block, arrOptions[i][j][k])) {//Überprüfung ob Option hier möglich ist
+                                    arrOptions[i][j][k] = 0;
+                                }
+                            }
+
+                        }
+                        only1Option(arrOptions, i, j); // überprüfung ob nur mehr eine Option über bleibt und Speicherung in Solutions
+                    }
                 }
 
             }
 
-        }
+            block = new int[9];
 
-        return new int[0][0]; // delete this line!
+            //Überprüfung der Spalten
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (this.solution[j][i] == 0) {
+                        for (int k = 0; k < 9; k++) {
+                            block[k] = this.solution[k][i];
+
+                        }
+
+                        for (int k = 0; k < this.solution.length; k++) {
+                            if (arrOptions[i][j][k] != 0) { //Überprüfung ob die Option nicht schon gelöscht wurde
+                                if (existenz(block, arrOptions[i][j][k])) {//Überprüfung ob Option hier möglich ist
+                                    arrOptions[i][j][k] = 0;
+                                }
+                            }
+
+                        }
+                        only1Option(arrOptions, i, j); // überprüfung ob nur mehr eine Option über bleibt und Speicherung in Solutions
+                    }
+                }
+
+            }
+
+            //Überprüfung der 9erBlöcke
+            block = new int[9];
+            int row = 0;
+            int spalte = 0;
+            int aktuelleStelle = 0;
+            for (int y = 0; y < solution.length; y++) {
+                for (int x = 0; x < solution.length; x++) {
+                    if (solution[y][x] == 0) { // überprüft ob Stelle schon fixen Wert hat
+
+                        if (y < 3) { //-> zuweisen der 9erBlock Bereiche
+                            row = 0;
+                        } else if (y < 6) {
+                            row = 3;
+                        } else {
+                            row = 6;
+                        }
+                        if (x < 3) {
+                            spalte = 0;
+                        } else if (x < 6) {
+                            spalte = 3;
+                        } else {
+                            spalte = 6;
+                        }
+                        aktuelleStelle = 0;
+
+                        for (int e = 0; e < 3; e++) {
+                            for (int i = 0; i < 3; i++) {
+                                block[aktuelleStelle] = solution[row][spalte];
+                                spalte++;
+                                aktuelleStelle++;
+                            }
+                            row++;
+                            spalte = spalte - 3;
+                        }
+                        for (int i = 0; i < solution.length; i++) {
+                            if (arrOptions[y][x][i] != 0) { //Überprüfung ob die Option nicht schon gelöscht wurde
+                                if (existenz(block, arrOptions[y][x][i])) {//Überprüfung ob Option hier möglich ist
+                                    arrOptions[y][x][i] = 0;
+                                }
+                            }
+
+                        }
+                        only1Option(arrOptions, y, x);
+                    }
+                }
+
+            }
+        }
+        return solution;
     }
 
-    private int[] checkOptions(int[] options, int[] block) {
-        for (int i = 0; i < options.length; i++) {
-            for (int j = 0; j < block.length; j++) {
-                if (block[j] == options[i]) {
-                    break;
+    @Override
+    public int[][] solveSudokuParallel(int[][] rawSudoku) {
 
-                }
-
-            }
-
-        }
-        return options;
+        return new int[0][0];
     }
 
     public long benchmark(int[][] rawSudoku) {
@@ -167,5 +242,32 @@ public class SudokuSolver implements ISodukoSolver {
         }
         long after = System.currentTimeMillis();
         return before - after / 10;
+    }
+
+    private boolean existenz(int[] block, int option) {
+        for (int i = 0; i < block.length; i++) {
+            if (block[i] == option) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    private void only1Option(int[][][] options, int stellei, int stellej) {
+        int count = 0; //zählt die Anzahl der Zahlen über 0
+        int stelle = 0;
+        for (int k = 0; k < options.length; k++) {
+            if (options[stellei][stellej][k] != 0) {
+                count++;
+                stelle = k;
+            }
+
+        }
+        if (count == 1) {
+            this.solution[stellei][stellej] = options[stellei][stellej][stelle];
+            System.out.println("Wert wurde gespreichert");
+        }
+
     }
 }
